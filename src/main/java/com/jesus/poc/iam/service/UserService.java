446cef6a4d6f309b0f028566726648003ce7d6cc
Pupo <pupo@tutanota.com>
2019-01-09 09:33:49 -0500

    Added UserService.java

---
+++ src/main/java/com/jesus/poc/iam/service/UserService.java
@@ -0,0 +1,25 @@
+package com.jesus.poc.iam.service;
+
+import com.jesus.poc.iam.domain.User;
+import com.jesus.poc.iam.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.jesus.poc.iam.domain.Sex;
+import java.util.Date;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(String firstName, String middleName, String lastName, Date dateOfBirth, Sex sex, Boolean active) {
+        return userRepository.save(
+                new User(firstName, middleName, lastName, dateOfBirth, sex, active)
+        );
+    }
+}
